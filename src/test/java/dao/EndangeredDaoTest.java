997a9cae5f64b7011a01a05c3b8481912678e1df
michael-kip <michael.kiprotich@student.moringaschool.com>
2022-02-14 06:42:37 +0300

    updating tests to check whether added animal can be found by id

--- src/test/java/dao/EndangeredDaoTest.java
+++ src/test/java/dao/EndangeredDaoTest.java
@@ -10,18 +10,20 @@ import static org.junit.Assert.*;
 public class EndangeredDaoTest {
     EndangeredDao endangeredDao = new EndangeredDao() {
         @Test
-        public void createAnimal()throws Exception {
+        public void addAnimal()throws Exception {
             EndangeredAnimals endangeredAnimals= new EndangeredAnimals("aro","heatlth","young","secB");
             int myid = endangeredAnimals.getId();
             endangeredAnimals.add(endangeredAnimals);
             assertNotEquals(myid,endangeredAnimals.getId());
         }
 
-        @Override
-        public List<EndangeredAnimals> findAll() {
-            return null;
+        @Test
+        public void createdAnimalCanBeFoundByfindById() throws Exception {
+            EndangeredAnimals endangeredAnimals= new EndangeredAnimals("aro","heatlth","young","secB");
+            endangeredAnimals.add(endangeredAnimals);
+            EndangeredAnimals foundEndangered = endangeredDao.findById(endangeredAnimals.getId());
+            assertEquals(endangeredAnimals,foundEndangered);
         }
-
         @Override
         public EndangeredAnimals findById(int id) {
             return null;
