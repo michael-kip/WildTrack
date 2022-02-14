cb1af8b21a86541b42e3beaf32e161094520d4f2
michael-kip <michael.kiprotich@student.moringaschool.com>
2022-02-14 06:40:12 +0300

    adding tests to check if animal is added successfully

--- src/test/java/dao/EndangeredDaoTest.java
+++ src/test/java/dao/EndangeredDaoTest.java
@@ -1,7 +1,40 @@
 package dao;
 
+import models.EndangeredAnimals;
+import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class EndangeredDaoTest {
+    EndangeredDao endangeredDao = new EndangeredDao() {
+        @Test
+        public void createAnimal()throws Exception {
+            EndangeredAnimals endangeredAnimals= new EndangeredAnimals("aro","heatlth","young","secB");
+            int myid = endangeredAnimals.getId();
+            endangeredAnimals.add(endangeredAnimals);
+            assertNotEquals(myid,endangeredAnimals.getId());
+        }
 
+        @Override
+        public List<EndangeredAnimals> findAll() {
+            return null;
+        }
+
+        @Override
+        public EndangeredAnimals findById(int id) {
+            return null;
+        }
+
+        @Override
+        public void deleteAll() {
+
+        }
+
+        @Override
+        public void deleteById(int id) {
+
+        }
+    }
 }
